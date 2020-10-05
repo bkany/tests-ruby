9efb31861cdfb196ba1ba2fa89a0c6e434628d52
bkany <bkany@student.42.fr>
2020-10-05 18:49:30 +0200

    Exercice 06 - almost done

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,4 +1,27 @@
+def add_ay(a)
+  return a + "ay"
+end
+
+def permutation(a, i)
+  str = Array.new()
+  k = 0
+  for k in 0..(a.length - 1)
+    str[k] = a[(k + i) % a.length] 
+  end
+  return str.join
+end
+
+def vowel(a)
+  for i in 0..(a.length - 1)
+    if a[i] =~ /[aeio]/i
+      return i
+    end
+  end
+  return (a.length - 1)
+end
+
 def translate(a)
-  index = a.index { |x| x == /[aeiou]/ }
+  b = vowel(a)
+  a = a.split.map{ |x| add_ay(permutation(x, b)) }.join(" ")
   return a
 end
